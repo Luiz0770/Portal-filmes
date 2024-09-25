78ede7419b23d1af3531f74b5a75cae394478a04
Luiz0770 <ramosluizfelipe104@gmail.com>
2024-09-24 21:36:31 -0300

    criando pagina de pesquisa

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,10 +1,40 @@
-export default function MovieListPage(){
-    return(
-        <>
+import { useState } from "react";
+import movies from "../data/movies.json";
+import MovieCard from "../components/MovieCard";
+
+export default function MovieListPage() {
+  const [pesquisa, setPesquisa] = useState("");
+
+  function aoDigitar(e) {
+    setPesquisa(e.target.value);
+    console.log(e.target.value);
+  }
+
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center">
         <h1>Movie List Page</h1>
-        {/* Exibe a lista de todos os filmes disponíveis categorizados por gênero.
-        Cada filme na lista é mostrado com uma imagem, 
-        título e um link para a página de detalhes do filme.*/}
-        </>
-    )
+        <input
+          type="text"
+          value={pesquisa}
+          onChange={aoDigitar}
+          className="border"
+        />
+      </div>
+
+      {movies
+        .filter(
+          (movie) =>
+            pesquisa === "" ||
+            movie.titulo.toLowerCase().includes(pesquisa.toLowerCase())
+        )
+        .map((movie) => (
+          <MovieCard
+            key={movie.id}
+            titulo={movie.titulo}
+            imagem_destaque={movie.imagem_destaque}
+          />
+        ))}
+    </>
+  );
 }
